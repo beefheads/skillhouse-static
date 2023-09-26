22652883e0ab89190d8a225b54d091a16eb4c66c
Boffeer <41575886+Boffeer@users.noreply.github.com>
2023-09-26 16:34:24 +0300

    делает компонент видео с ютуба

---
+++ src/js/components.b/spawners/_b_video.js
@@ -0,0 +1,239 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  if (document.querySelector('.b_video') == null) return;
+
+  const CLASSES = {
+  	inited: 'b_video--inited',
+  	markedup: 'b_video--marked-up',
+    videoStarted: 'b_video--started',
+    playing: 'b_video--playing',
+    hasAutoplay: 'b_video--has-autoplay',
+    looped: 'b_video--looped',
+    ready: 'b_video--ready',
+  }
+
+  function getVideoSource(url) {
+    if (url.includes('youtube')) {
+      return 'youtube';
+    } else if (url.includes('vimeo')) {
+      return 'vimeo';
+    } else {
+      return 'regular';
+    }
+  }
+
+  function initVideos(videoElementsArray) {
+    videoElementsArray.forEach((videoElement, index) => {
+      if (videoElement.classList.contains(CLASSES.inited)) return;
+
+      const videoUrl = videoElement.dataset.videoUrl;
+      const source = getVideoSource(videoUrl);
+      switch (source) {
+        case 'youtube':
+          initYoutubeVideo(videoElement, index);
+          break;
+        case 'vimeo':
+          console.log('vimeo')
+          break;
+        default:
+          console.log('regular')
+          break;
+      }
+
+      videoElement.classList.add(CLASSES.inited)
+    });
+  }
+
+
+  function initYoutubeVideo(videoElement, index) {
+      const ytId = new URL(videoElement.dataset.videoUrl).searchParams.get('v');
+
+      makeVideoMarkup(videoElement, index);
+      setVideoThumbs(videoElement, ytId);
+  }
+
+  function getVideoThumb(id) {
+    const thumbs = {
+      thumb: `https://i.ytimg.com/vi/${id}/maxresdefault.jpg`,
+      webp: `https://i.ytimg.com/vi_webp/${id}/maxresdefault.webp`,
+      // thumb: `https://i.ytimg.com/vi/${id}/hqdefault.jpg`,
+      // webp: `https://i.ytimg.com/vi_webp/${id}/hqdefault.webp`,
+    }
+    return thumbs;
+  }
+  function setVideoThumbs(videoElement, id) {
+    const {thumb, webp} = getVideoThumb(id);
+    videoElement._b_video.img.src = thumb;
+    videoElement._b_video.webp.srcset = webp;
+    videoElement._b_video.thumbs = {
+      thumb,
+      webp,
+    }
+    videoElement._b_video.id = id;
+  }
+
+  function makeVideoMarkup(videoElement, index) {
+    if (videoElement.classList.contains(CLASSES.markedup)) return;
+
+    let cover = document.createElement('div');
+    cover.classList.add('b_video__cover', 'js_spawned');
+
+    let pic = document.createElement('picture');
+    pic.classList.add('b_video__cover-pic', 'js_spawned');
+
+    let img = document.createElement('img');
+    img.classList.add('b_video__cover-img', 'js_spawned');
+
+    let picWebp = document.createElement("source");
+    picWebp.type = "image/webp";
+
+    let media = document.createElement('div');
+    media.classList.add('b_video__media', 'js_spawned');
+
+    let videoFrame = document.createElement('div');
+    videoFrame.classList.add('b_video__player', 'js_spawned');
+    videoFrame.id = `b_video-${index}`;
+
+    pic.appendChild(picWebp);
+    pic.appendChild(img);
+    cover.appendChild(pic);
+
+    media.appendChild(videoFrame);
+
+    videoElement.appendChild(cover);
+    videoElement.appendChild(media);
+
+    videoElement._b_video = {
+      player: {},
+      pic,
+      webp: picWebp,
+      img,
+      media,
+      videoFrame,
+    };
+
+    if (!videoElement.classList.contains('b_video--bg')) {
+      const playButton = document.createElement('button')
+      playButton.classList.add('b_video__play', 'js_spawned');
+      videoElement.appendChild(playButton);
+
+      playButton.addEventListener('click', () => {
+        toggleVideo(videoElement)
+      });
+
+      videoElement._b_video.play = playButton;
+    }
+    videoElement.classList.add(CLASSES.markedup);
+  }
+
+  function playVideo(videoElement) {
+    if (videoElement.classList.contains(CLASSES.videoStarted)) {
+      videoElement.classList.add(CLASSES.videoStarted);
+    }
+
+    videoElement._b_video.player.playVideo();
+    videoElement.classList.add(CLASSES.playing)
+  }
+
+  function pauseVideo(videoElement) {
+    videoElement.classList.remove(CLASSES.playing)
+    setTimeout(() => {
+      videoElement._b_video.player.pauseVideo();
+    }, 100)
+  }
+
+  function toggleVideo(videoElement) {
+    if (videoElement.classList.contains(CLASSES.playing)) {
+      pauseVideo(videoElement)
+      return;
+    }
+
+    playVideo(videoElement);
+  }
+  
+  function initPlayers(videos) {
+    videos.forEach(video => {
+      let hasAutoplay = 0;
+      let isMuted = 0;
+      if (video.classList.contains(CLASSES.hasAutoplay)) {
+        hasAutoplay = 1;
+        isMuted = 1;
+      }
+
+      // const videoElementId = video.id;//`b_video-${index}`
+      video._b_video.player = new YT.Player(video._b_video.videoFrame.id, {
+        videoId: video._b_video.id,
+        events: {
+          onReady: onPlayerReady,
+          onStateChange: cahngePlayerState,
+        },
+        playerVars: {
+          autoplay: hasAutoplay,
+          controls: 0,
+          disablekb: 1,
+          showinfo: 0,
+          modestbranding: 1,
+          fs: 0,
+          cc_load_policy: 0,
+          iv_load_policy: 3,
+          autohide: 1,
+          playsinline: 1,
+          mute: isMuted,
+          loop: 1,
+          rel: 0,
+          version: 3,
+          origin: window.location.href,
+        },
+      });
+    });
+  }
+
+  function onPlayerReady(e) {
+    const videoId = e.target.h.g.videoId
+    const video = document.querySelector(`[data-video-url*="${videoId}"`);
+
+    if (video.classList.contains(CLASSES.hasAutoplay)) {
+      video._b_video.player.setVolume(40);
+      video._b_video.player.playVideo();
+      setTimeout(() => {
+        video.classList.add(CLASSES.playing, CLASSES.videoStarted);
+      }, 1000);
+    }
+
+    video.classList.add(CLASSES.ready);
+  }
+
+  function cahngePlayerState(e) {
+    const videoId = e.target.h.g.videoId
+    const video = document.querySelector(`[data-video-url*="${videoId}"`);
+
+    // show video when loaded instead of thumb
+    if (e.data == 1) {
+      // document.querySelector(".b_video__media").style.display = "block";
+    } else if (e.data == 0) {
+      // document.querySelector(".b_video__media").remove();
+    }
+
+    // loop video
+    if (video.classList.contains(CLASSES.looped)) {
+      if (e.data === YT.PlayerState.ENDED) {
+        video._b_video.player.playVideo();
+      }
+    }
+  }   
+
+  const videos = document.querySelectorAll('.b_video')
+  initVideos(videos);
+
+  // Load the IFrame Player API code asynchronously.
+  var tag = document.createElement("script");
+  tag.src = "https://www.youtube.com/player_api";
+  var firstScriptTag = document.getElementsByTagName("script")[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // Replace the 'ytplayer' element with an <iframe> and
+  // YouTube player after the API code downloads.
+  function onYouTubePlayerAPIReady() {
+    initPlayers(videos)
+  }
+  window.onYouTubePlayerAPIReady = onYouTubePlayerAPIReady;
+});
--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,7 @@ import "./helpers.b/_quickfix.js"
 import "./components.b/header/header.js";
 import "./components.b/controls/formich.js";
 import "./components.b/spawners/b_modal.js";
+import "./components.b/spawners/_b_video.js";
 // import "./components.b/controls/_button-sticky.js";
 
 import "./components.b/controls/b_tabs.js";
